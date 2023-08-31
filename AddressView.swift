3463049a85079e6bae6c4f3a3366f8c6c47c1b16
Eugene Cross <eugene.cross@gmail.com>
2023-08-31 16:29:04 +0100

    HWS Project 10, Days 49 - 51: Cupcake Corner

---
+++ AddressView.swift
@@ -0,0 +1,20 @@
+//
+//  AddressView.swift
+//  CupcakeCorner
+//
+//  Created by Eugene on 30/08/2023.
+//
+
+import SwiftUI
+
+struct AddressView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddressView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddressView()
+    }
+}
---
+++ Order.swift
@@ -0,0 +1,8 @@
+//
+//  Order.swift
+//  CupcakeCorner
+//
+//  Created by Eugene on 30/08/2023.
+//
+
+import Foundation
